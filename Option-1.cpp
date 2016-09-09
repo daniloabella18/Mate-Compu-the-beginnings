9087985deaf7bb3e8e2ef089a682c28c0a38f193
Danilo <daniloabella@gmail.com>
2016-09-09 12:09:19 -0300

    // Sex ( Julia )

---
+++ Option-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <gmp.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  int n;
+
+  std::cout << "/* Ingrese n!: */" << std::endl;
+  std::cin >> n;
+  std::cout << n! << std::endl;
+  return 0;
+}
--- test.cpp
+++ test.cpp
@@ -12,11 +12,12 @@ int main(int argc, char *argv[])
     printf("Please supply two numbers to add.\n");
     return 1;
   }
-  
+
   mpz_inist_set_str (a, argv[1], 10);	/* Assume decimal integers */
   mpz_init_set_str (b, argv[2], 10);	/* Assume decimal integers */
   mpz_add (a, a, b);			/* a=a+b */
 
   printf("%s + %s => %s\n", argv[1], argv[2], mpz_get_str (NULL, 10, a));
   return 0;
+  //sex
 }
