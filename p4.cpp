6b8d5f4349c84a1e781b51050a773a958acdf5f3
Danilo <daniloabella@gmail.com>
2016-09-17 18:20:20 -0300

    Trabajo finalizado

--- p4.cpp
+++ p4.cpp
@@ -1,3 +1,7 @@
+// Danilo Abellá - Sergio Salinas
+// LCC - USACH
+// Xubuntu
+
 #include <iostream>
 #include <gmp.h>
 #include <cstdlib>
@@ -13,48 +17,46 @@ void factorial(mpf_t n, mpf_t r, mpf_t r_result, mpf_t n_result, mpf_t comb, mpf
   while( i > 0 )
   { 
 
-//-----------------------------------------------// Comparaciones
+    i=mpf_cmp_ui (r, 1);		// Da un valor a "i" dependiendo si "r" es <1, =1 o >1.
 
-    i=mpf_cmp_ui (r, 1);
+//--------------------------------------------------------------------------//
 
-//-----------------------------------------------//
-
-    mpf_init_set(r_result, r); //r_result = i
+    mpf_init_set(r_result, r); 			//r_result = r
 
     cout<<"\nr=";
 
-    mpf_out_str(stdout,10,100,r); 
+    mpf_out_str(stdout,10,100,r); 		// Muestra r
   
     cout<<"\nr_r:" ;
 
-    mpf_out_str(stdout,10,100,r_result);
+    mpf_out_str(stdout,10,100,r_result);  	// Muestra r_result
 
-//-----------------------------------------------//
+//--------------------------------------------------------------------------//
     
-    mpf_init_set(n_result, n); //n_result = j
+    mpf_init_set(n_result, n); 			//n_result = n
 
     cout<<"\nn=";
 
-    mpf_out_str(stdout,10,100,n); 
+    mpf_out_str(stdout,10,100,n); 		// Muestra n
   
     cout<<"\nn_r:" ;
 
-    mpf_out_str(stdout,10,100,n_result);
+    mpf_out_str(stdout,10,100,n_result);  	// Muestra n_result
 
-//-----------------------------------------------//
+//--------------------------------------------------------------------------//
 
-    mpf_sub_ui(r,r,1); mpf_sub_ui(n,n,1);
+    mpf_sub_ui(r,r,1); mpf_sub_ui(n,n,1);	// Resta valores a "r" y "n".
  
-    mpf_div(nr_result,n_result,r_result ); // n/r_result = n_result / r_result
+    mpf_div(nr_result,n_result,r_result ); 	// nr_result = n_result / r_result
 
     cout<<"\nnr_r: ";
 
-    mpf_out_str(stdout,10,100,nr_result);
+    mpf_out_str(stdout,10,100,nr_result);	// Muestra nr_result
 
     mpf_mul(comb,comb,nr_result); //comb = comb * n/r_result
   
     cout<<"\ncomb: ";
-    mpf_out_str(stdout,10,100,comb);
+    mpf_out_str(stdout,10,100,comb);		// Muestra comb
 
     cout<<"\nCiclo\n";
    }
@@ -63,16 +65,18 @@ void factorial(mpf_t n, mpf_t r, mpf_t r_result, mpf_t n_result, mpf_t comb, mpf
 
 int main(int argc, char *argv[]) 
 {
+//--------------/ Evalua que el usuario coloque al menos 2 numeros //--------//
   if (argc<3)
   {					
      printf("Please supply two numbers to add.\n");
      return 1;
   }
+//--------------------------------------------------------------------------//
   clock_t start, end;
-  start = clock();
+  start = clock();				// Toma tiempo inicial
 
   mpf_t n,r;
-
+// Transforma valores ingresados como variables mpf_t.
   mpf_init_set_str (r, argv[2], 10);
   mpf_init_set_str (n, argv[1], 10);
 
@@ -82,22 +86,27 @@ int main(int argc, char *argv[])
   mpf_t nr_result;
   mpf_t comb;
   /* Inicia variables y les asigna un número */
-  mpf_init_set_ui(n_result, 1); //inicia puntero y le inicia valor
+  mpf_init_set_ui(n_result, 1);
   mpf_init_set_ui(r_result, 1);
   mpf_init_set_ui(nr_result, 1);
   mpf_init_set_ui(comb,1);
 
-  factorial( n,r,r_result,n_result,comb,nr_result );
+  factorial( n,r,r_result,n_result,comb,nr_result );    // Factorial de divisiones
+
+//--------------/ Combinatoria /----------------//
 
   cout<<"\nCombinatoria:";
 
   mpf_out_str(stdout,10,10,comb);
-
+// Elimina punteros
   mpf_clear(n_result);
   mpf_clear(r_result);
   mpf_clear(nr_result);
   mpf_clear(comb);
-  end = clock();
-  cout<<"\nTiempo: "; cout<<((double)(end-start))/CLOCKS_PER_SEC; cout<<"\n\n";
+
+  end = clock();              			// Toma tiempo final
+
+  cout<<"\nTiempo: "; cout<<((double)(end-start))/CLOCKS_PER_SEC; cout<<"\n\n";   // Calcula tiempo usado y lo muestra
+ 
   return 0;
 }
