81dd7d1621ff3049bc6bee33c9b2173d0583f37a
Sergio Salinas <sergiosalfer@gmail.com>
2016-09-19 05:15:58 -0300

    agregado time a main.cpp, falta arreglar salida p4.cpp, p5.cpp y hacer p3.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,40 @@
-#include <iostream>
-#include <gmp.h>
+
 #include <cstdlib>
+#include <gmp.h>
+#include <iostream>
 #include "p1.cpp"
 #include "p2.cpp"
+#include "p3.cpp"
+/* Estos no pueden ser agregados hasta que se arregle la salida"
+   #include "p4.cpp"
+   #include "p5.cpp"
+ */
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-
-        switch (atoi(argv[3])) {
-        case 1:
-                p1(argc,argv);
-                break;
-        case 2:
-                p2(argc,argv);
-                break;
-        case 3:
-                p3(argc,argv);
-                break;
-        case 4:
-                p4(argc,argv);
-                break;
-        case 5:
-                p5(argc,argv);
-                break;
+        if(argc < 4) {
+                cout<<"Insufficient arguments";
+        }else{
+                clock_t tStart = clock();
+                double dif;
+                switch (atoi(argv[3])) {
+                case 1:
+                        p1(argc, argv);
+                        break;
+                case 2:
+                        p2(argc, argv);
+                        break;
+                case 3:
+                        // p3(argc,argv);
+                        break;
+                case 4:
+                        // p4(argc,argv);
+                        break;
+                case 5:
+                        // p5(argc,argv);
+                        break;
+                }
+                printf("\t%.7f\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
         }
-
         return 0;
 }
--- p1.cpp
+++ p1.cpp
@@ -1,16 +1,14 @@
-#include <iostream>
-#include <gmp.h>
 #include <cstdlib>
+#include <gmp.h>
+#include <iostream>
 using namespace std;
 
-
-void factorial(mpz_t n, mpz_t result){
+void factorial(mpz_t n, mpz_t result) {
         mpz_t i;
-        for (mpz_init_set_ui(i, 1); mpz_cmp(i,n) <= 0; mpz_add_ui(i, i, 1)) {
-                mpz_mul(result,result,i); //result = result * i
+        for (mpz_init_set_ui(i, 1); mpz_cmp(i, n) <= 0; mpz_add_ui(i, i, 1)) {
+                mpz_mul(result, result, i); // result = result * i
         }
 }
-
 
 int p1(int argc, char const *argv[]) {
         /* Asigna punteros */
@@ -22,23 +20,24 @@ int p1(int argc, char const *argv[]) {
         mpz_t nr_result;
         mpz_t comb;
         /* Inicia variables y les asigna un número */
-        mpz_init_set_str(n, argv[1],10);
-        mpz_init_set_str(r, argv[2],10);
+        mpz_init_set_str(n, argv[1], 10);
+        mpz_init_set_str(r, argv[2], 10);
         mpz_init(nr);
-        mpz_sub(nr ,n , r); //nr = n-r
-        mpz_init_set_ui(n_result, 1); //inicia puntero y le inicia valor
+        mpz_sub(nr, n, r);      // nr = n-r
+        mpz_init_set_ui(n_result, 1); // inicia puntero y le inicia valor
         mpz_init_set_ui(r_result, 1);
         mpz_init_set_ui(nr_result, 1);
-        mpz_init_set_ui(comb,0);
+        mpz_init_set_ui(comb, 0);
 
-        factorial(n,n_result);
-        factorial(r,r_result);
+        factorial(n, n_result);
+        factorial(r, r_result);
         factorial(nr, nr_result);
 
-        mpz_mul(comb,r_result,nr_result); // Multiplica dos mpz, si tiene _ui multiplica un mpz y una constante
-        mpz_cdiv_q(comb,n_result, comb );
+        mpz_mul(comb, r_result, nr_result); // Multiplica dos mpz, si tiene _ui
+                                            // multiplica un mpz y una constante
+        mpz_cdiv_q(comb, n_result, comb);
 
-        mpz_out_str(stdout,10,comb);
+        mpz_out_str(stdout, 10, comb);
 
         mpz_clear(n_result);
         mpz_clear(r_result);
