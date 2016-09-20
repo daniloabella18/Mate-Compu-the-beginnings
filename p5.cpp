cb79d7909624028ca5d7b713bdc01c0acf53452e
Danilo <daniloabella@gmail.com>
2016-09-19 21:47:33 -0300

    Muestra SOLO resultado final

--- p5.cpp
+++ p5.cpp
@@ -49,8 +49,6 @@ void taylor ( mpf_t n, mpf_t n_el, mpf_t div_taylor, mpf_t denom, mpf_t r, mpf_t
 
                 mpf_add( denom, denom, div_taylor ); // denom = denom + div_taylor
         }
-
-        cout<<"\ndenominador: e^n=";  mpf_out_str(stdout,10,100,denom);  cout<<"\n";
 }
 
 
@@ -60,15 +58,12 @@ void taylor ( mpf_t n, mpf_t n_el, mpf_t div_taylor, mpf_t denom, mpf_t r, mpf_t
 
 int main(int argc, char *argv[])
 {
-        if (argc<3)
+   /*     if (argc<3)
         {
                 printf("Please supply two numbers to add.\n");
                 return 1;
         }
-
-        clock_t start, end;
-        start = clock();
-
+   */
         float n_pow, potencia, numerador_n;
 
         mpf_t n,r,n_el;
@@ -102,45 +97,32 @@ int main(int argc, char *argv[])
 //----------/ Numerador /-----------------------------------------------------------------//
 
         n_pow = mpf_get_ui(n_el);                                 // Pasaje a int;
-
-        cout<<"\nn_pow: ((n+1)/2)=";  cout<<n_pow;  cout<<"\n";
 //-------------------------------------------------------------//
         potencia = (n_pow+1)/2;         // Pasaje a float;
-
-        cout<<"\npotencia: n^((n+1)/2)=";  cout<<potencia;  cout<<"\n";
 //---------------------------------------------------------------------------------------//
         numerador_n = pow( n_pow, potencia );                   // Calcular potencia;
-
-        cout<<"\nnumerador_n: n^((n+1)/2)=";  cout<<numerador_n;  cout<<"\n";
-
-
 //----------/Denominador /-----------------------------------------------------------------//
 
         taylor ( n, n_el, div_taylor, denom, r, r_result, fac );
 
         mpf_ui_div( div, numerador_n, denom );                        // div = numerador_n / denom
 
-        cout<<"\ndiv: ( n^((n+1)/2) / e^n ) ="; mpf_out_str(stdout,10,100,div);  cout<<"\n";
-
 //========================// Raiz //==========================--------------------------//
 
         mpf_init_set_d( dospi, 6.28318530717958647692528676655 );
 
-        cout<<"\ndospi: "; mpf_out_str(stdout,10,100,dospi);  cout<<"\n";
-
         mpf_sqrt ( raiz, dospi );
-
-        cout<<"\nraiz de dospi: "; mpf_out_str(stdout,10,100,raiz);  cout<<"\n";
-
 
 //========================// Resultado final //==========================--------------------------//
 
 
         mpf_mul(comb,raiz,div); //fac = fac * r_result
 
-        cout<<"\nk!(apróx):";
+ //     cout<<"\nk!(apróx):";
 
         mpf_out_str(stdout,10,10,comb); // Muestra resultado final
+
+ //     cout<<"\n"<<endl;
 
         mpf_clear(r_result);
         mpf_clear(n);
@@ -154,9 +136,5 @@ int main(int argc, char *argv[])
         mpf_clear(dospi);
         mpf_clear(raiz);
 
-
-        end = clock();
-
-        cout<<"\nTiempo: "; cout<<((double)(end-start))/CLOCKS_PER_SEC; cout<<"\n\n";
         return 0;
 }
