a3592483de52943e7f9437ad1034039ec3270305
Sergio Salinas <sergiosalfer@gmail.com>
2016-09-27 17:31:44 -0300

    agregada funcion nfijo y rfijo para el script de compilación

--- data.sh
+++ data.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-r=2
-g++ main.cpp -lgmp
-for i in {4..100}
-do
-  ./a.out $i $r  1
-done
+
+
+#r fijo, n creciente
+function rfijo {
+  for (( i=$2; i<=$1; i++ ))
+  do
+    ./a.out $i $2  1 >> data.txt
+  done
+
+}
+
+#n fijo y r crece hasta r
+function nfijo {
+  for (( i=$2; i<=$1; i++ ))
+  do
+    ./a.out $1 $i  1 >> data.txt
+  done
+}
+
+rfijo 100 3
+#gnuplot -persist <<-EOFMarker
+# set title "Tu hermana " font ",14" textcolor rgbcolor "royalblue"
+#    plot "data.txt" using 1:2 with lines
+#EOFMarker
+#Para borrar todo
+#echo -n "" > data.txt
+# g++ main.cpp -lgmp
