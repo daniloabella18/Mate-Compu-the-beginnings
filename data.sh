85fa54ba72af8151254bcdc0697bac70588c4924
Sergio Salinas <sergiosalfer@gmail.com>
2016-09-27 17:44:06 -0300

    agregado menu para Danilo

--- data.sh
+++ data.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+#g++ main.cpp -lgmp && ./a.out 4 3 1
 
-
+#n fijo y r crece hasta r
+function nfijo {
+  for (( i=$2; i<=$1; i++ ))
+  do
+    ./a.out $1 $i  1 >> data.txt
+  done
+}
 
 #r fijo, n creciente
 function rfijo {
@@ -11,15 +18,17 @@ function rfijo {
 
 }
 
-#n fijo y r crece hasta r
-function nfijo {
-  for (( i=$2; i<=$1; i++ ))
-  do
-    ./a.out $1 $i  1 >> data.txt
-  done
-}
 
-rfijo 100 3
+
+echo "Elija una opción"
+echo "1. Graficar para n fijo y r crece hasta n"
+echo "2. Graficar para r fijo y y que crezca desde r hasta n"
+read opcion
+if [[ "$opcion" == 1 ]]; then
+  nfijo 100 3
+elif [[ "$opcion" == 2 ]]; then
+  rfijo 100 3
+fi
 #gnuplot -persist <<-EOFMarker
 # set title "Tu hermana " font ",14" textcolor rgbcolor "royalblue"
 #    plot "data.txt" using 1:2 with lines
