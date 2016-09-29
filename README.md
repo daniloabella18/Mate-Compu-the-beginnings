fc26a843c59cdd4cbb898671ecd4af97d113ba5b
Sergio Salinas <sergiosalfer@gmail.com>
2016-09-29 00:17:14 -0300

    actualizado readme

--- README.md
+++ README.md
@@ -6,19 +6,22 @@ Se compila usando el siguiente codigo en la terminal, se requiere la librería g
 
   `g++ main.cpp -lgmp`
 
-Se ejecuta coN
+Se ejecuta con
 
   `./a.out n r metodo`
 
 Donde n y r son los valores de la función C(n,r) y metodo el método elegido para encontrar el resultado de la función.
 
 ##Ejecuntado script
-
-Aun no se ha agregado menu, por lo que los valores se deben modificar desde dentro del script.
+El script para gráficar se debe ejecutar con
 
   `bash data.sh`
 
-##TODO
-  - [x] Crear readme
-  - [ ] Agregar los metodos al readme
-  - [ ] Agregar salida gnu plot al script
+Esto va a generar 3 tipos de gráficos de disintos.
+
+ - n fijo y r crece hasta n
+ - r fijo y n va desde r hasta n
+ - n fijo y r siendo constantemente multiplicado por k hasta n
+
+
+Link del repositorio git: https://github.com/daniloabella18/Mate-Compu-the-beginnings
--- data.sh
+++ data.sh
@@ -29,7 +29,6 @@ EOFMarker
 #r fijo, n creciente
 #n=$1 r=$2 $3=metodo
 function rFijo {
-  echo -n "" > Informe/Metodo$3/data1.txt
   for (( i=$2; i<=$1; i++ ))
   do
     echo $(./a.out $i $2  $3) $i >> Informe/Metodo$3/data2.txt
@@ -51,11 +50,10 @@ EOFMarker
 #n=$1 r=$2 $3=k  $4=metodo
 #n fijo y r esta siendo constantemente multiplicado por k hasta n
 function nFijokVaria {
-  echo -n "" > Informe/Metodo$4/data3.txt
-  for (( i=$2; i<=$1; i = i*$3 ))
-    do
-      echo $(./a.out $1 $i $4) $i >> Informe/Metodo$4/data3.txt
-    done
+for (( i=$2; i<=$1; $i=i*$3 ))
+  do
+    echo $(./a.out $i $2  $4) $i >> Informe/Metodo$3/data3.txt
+  done
   gnuplot -persist <<-EOFMarker
    set title "Metodo $4 \n{/*0.8 r*k creciendo hasta n}" font ",14" textcolor rgbcolor "royalblue"
    set nokey
@@ -65,18 +63,71 @@ function nFijokVaria {
    set format y "%.7f"
    set term png
    set output "Informe/Metodo$3/plot3m$4.png"
-      plot "Informe/Metodo$4/data3.txt"  using 3:2 with lines
+      plot "Informe/Metodo$3/data3.txt"  using 3:2 with lines
 EOFMarker
 }
 
-# echo "Elija una opción"
-# echo "1. Graficar para n fijo y r crece hasta n"
-# echo "2. Graficar para r fijo y n va desde r hasta n"
-# echo "3. Graficar para n fijo y r siendo constantemente multiplicado por k hasta n"
-echo "Se están haciendo los gráficos, no me cierres"
-for i in 5 ; do
-      n=1000
-      r=1
-    #  read m
-      nFijo $n $r $i
-done
+# #n=$1 r=$2 $3=k  $4=metodo
+# #n fijo y r siendo constantemente multiplicado por k hasta n
+# function rFijokVaria {
+# for (( i=$2; i<=$1; i=i*$3 ))
+#   do
+#     ./a.out $1 $i $4>> data.txt
+#   done
+# }
+#
+# #r fijo con r siendo multiplicado por k hasta n
+# #n=$1 r=$2 $3=k  $4=metodo
+# function rFijokVaria {
+# for (( i=$2; i<=$1; i=i*$3 ))
+#   do
+#     ./a.out $i $2  $4>> data.txt
+#   done
+# }
+#
+# #n y r crecen recorriendo todas las combinaciones posibles
+# #n=$1  metodo=$2
+# function nVariarVaria {
+# for (( i=0; i<=$1; i++ ))
+#   do
+#     for (( j = 0 ; j <= i; j++ )); do
+#       ./a.out $i $j $2>> data.txt
+#     done
+#   done
+# }
+
+echo "Elija una opción"
+echo "1. Graficar para n fijo y r crece hasta n"
+echo "2. Graficar para r fijo y n va desde r hasta n"
+echo "3. Graficar para n fijo y r siendo constantemente multiplicado por k hasta n"
+#echo "4. Graficar para r fijo con r siendo multiplicado por k hasta n"
+#echo "5. Graficar para n y r crecen recorriendo todas las combinaciones posibles"
+read opcion
+#borra todo en data.txt
+if [[ "$opcion" == 1 ]]; then
+  echo "Ingrese método combinatorio a utilizar"
+  n=1000
+  r=1
+  read m
+  nFijo $n $r $m
+elif [[ "$opcion" == 2 ]]; then
+  echo "método combinatorio a utilizar"
+  n=1000
+  r=150
+  read m
+  rFijo $n $r $m
+elif [[ "$opcion" == 3 ]]; then
+  echo "Ingrese k y método combinatorio a utilizar"
+  n=10000
+  r=1
+  read n r k m
+  nFijokVaria $n $r $k $m
+# elif [[ "$opcion" == 4 ]]; then
+#   echo "Ingrese n, r, k y método combinatorio a utilizar"
+#   read n r k m
+#   nFijokVaria $n $r $k $m
+# elif [[ "$opcion" == 5 ]]; then
+#   echo "Ingrese n y método combinatorio a utilizar"
+#   read n r m
+#   nVariarVaria $n $r $m
+fi
